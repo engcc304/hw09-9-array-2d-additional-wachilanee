ff306e1ff0ed7b15a8d678414a3e681e7887e384
wachilanee <wachilanee47@gmail.com>
2023-09-21 00:26:07 +0700

    Hw9-9

--- Homework/HW09/hw09-9-array-2D-additional.cpp
+++ Homework/HW09/hw09-9-array-2D-additional.cpp
@@ -21,3 +21,53 @@
             8 9 10
 
 */
+
+#include <stdio.h>
+
+int main() {
+    
+    int i, j, row1, cols1, row2, cols2;
+
+    printf("Array1 element :") ;
+    scanf( "%d %d", &row1, &cols1 ) ;
+
+    int matrix[ row1 ][ cols1 ] ;
+    
+    printf( "Input :" ) ;
+    for ( i = 0 ; i < row1 ; i++ ){
+        for ( j = 0 ; j < cols1 ; j++ ) {
+            scanf( "%d", &matrix[ i ][ j ] ) ;
+        }
+    }
+
+    printf("Array2 element :") ;
+    scanf( "%d %d", &row2, &cols2 ) ;
+
+    int matrix2[ row2 ][ cols2 ] ;
+    
+    printf( "Input :" ) ;
+    for ( i = 0 ; i < row2 ; i++ ){
+        for ( j = 0 ; j < cols2 ; j++ ) {
+            scanf( "%d", &matrix2[ i ][ j ] ) ;
+        }
+    }
+
+    int matrix3[ row1 ][ cols1 ] ;
+    
+    // printf( "Input :" ) ;
+    for ( i = 0 ; i < row1 ; i++ ){
+        for ( j = 0 ; j < cols1 ; j++ ) {
+             matrix3[ i ][ j ] = matrix[ i ][ j ] + matrix2[ i ][ j ];
+        }
+    }
+
+    printf( "Array1 + Array2 = Array3\n   " ) ;
+    for ( i = 0 ; i < row1 ; i++ ) {
+            for ( j = 0 ; j < cols1 ; j++ ) {
+                printf( " %d", matrix3[ i ][ j ] ) ;
+            }
+            printf("\n   ");
+        }
+
+    return 0;
+}
