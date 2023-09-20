fdfa29fa6be08c02345ebacf3cda0ae20fd2a7c1
wachilanee <wachilanee47@gmail.com>
2023-09-21 00:31:00 +0700

    Hw9-9

--- Homework/HW09/hw09-9-array-2D-additional.cpp
+++ Homework/HW09/hw09-9-array-2D-additional.cpp
@@ -64,7 +64,7 @@ int main() {
     printf( "Array1 + Array2 = Array3\n" ) ;
     for ( i = 0 ; i < row1 ; i++ ) {
             for ( j = 0 ; j < cols1 ; j++ ) {
-                printf( " %d", matrix3[ i ][ j ] ) ;
+                printf( "%d ", matrix3[ i ][ j ] ) ;
             }
             printf("\n");
         }
